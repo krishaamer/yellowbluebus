e97372802ec3ba09607a6f46616331d2ca5639cb
Kris Haamer <kris@haam.co>
2018-06-10 15:44:53 +0300

    Try to disable hls

--- ybb-client/client/views/video/video_item.js
+++ ybb-client/client/views/video/video_item.js
@@ -1,12 +1,12 @@
 Template.video_item.onRendered(() => {
 
   self = Template.instance();
-  console.log(self);
+  //console.log(self);
   player = self.data.mp;
   if (player) {
 
-    player.source(self.data.films.file, { sourceTypes: ['hls', 'mp4', 'webm', 'mov'] }).play();
-    player.on('sourcechanged', () => console.log("sourcechanged"));
+    player.source(self.data.films.file, { sourceTypes: ['mp4', 'ogv', 'webm', 'mov'] }).play();
+    //player.on('sourcechanged', () => console.log("sourcechanged"));
   }
 });
 
