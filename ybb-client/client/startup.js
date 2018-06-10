d9f1d0586cb868c27a3fa9d2331949b27578cdbe
Kris Haamer <kris@haam.co>
2018-06-10 14:57:46 +0300

    Fix missing sessions

--- ybb-client/client/startup.js
+++ ybb-client/client/startup.js
@@ -21,30 +21,40 @@ keyboardJS.bind('w', function(e) {
 });
 
 keyboardJS.bind('1', function(e) {
+
+	Session.set('current-keyword', ['first']);
   	Session.set('current-phrase', 'Hi, chilling out with my friends. I miss you!');
    	Session.set('current-avatar', 'boy');
    	Session.set('start-speaking', false);
 });
 
 keyboardJS.bind('2', function(e) {
+
+	Session.set('current-keyword', ['second']);
 	Session.set('current-phrase', 'Miss you too.');
    	Session.set('current-avatar', 'girl');
    	Session.set('start-speaking', false);
 });
 
 keyboardJS.bind('3', function(e) {
+
+	Session.set('current-keyword', ['third']);
 	Session.set('current-phrase', 'I wanna see you tonight');
    	Session.set('current-avatar', 'boy');
    	Session.set('start-speaking', false);
 });
 
 keyboardJS.bind('4', function(e) {
+
+	Session.set('current-keyword', ['fourth']);
 	Session.set('current-phrase', 'Come to my place');
 	Session.set('current-avatar', 'girl');
 	Session.set('start-speaking', false);
 });
 
 keyboardJS.bind('5', function(e) {
+
+	Session.set('current-keyword', ['fifth']);
 	Session.set('current-phrase', "Heading out. I'll be there soon!");
 	Session.set('current-avatar', 'boy');
 	Session.set('start-speaking', false);
