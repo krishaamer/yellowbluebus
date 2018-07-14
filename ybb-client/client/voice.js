1d0e600b109e67d773fe70650b382d94b7ed10fb
Kris Haamer <kris@haam.co>
2018-07-14 12:32:21 +0300

    Add more phrases

--- ybb-client/client/voice.js
+++ ybb-client/client/voice.js
@@ -13,6 +13,14 @@ export function voice () {
 	          	Session.set('current-avatar', 'boy');
 	          	Session.set('start-speaking', false);
 	      	},
+	      	'Start' () {
+
+	          	FlowRouter.go('speak');
+	          	Session.set('current-phrase', 'Hi, chilling out with my friends. I miss you!');
+	          	Session.set('current-keyword', ['first']);
+	          	Session.set('current-avatar', 'boy');
+	          	Session.set('start-speaking', false);
+	      	},
 	      	'Start the bus' () {
 
 	          	FlowRouter.go('speak');
@@ -72,6 +80,61 @@ export function voice () {
 		        Session.set('current-avatar', 'boy');
 		        Session.set('start-speaking', false);
 		    },
+		    'I miss you so much' () {
+
+				Session.set('current-phrase', 'I miss you so much');
+				Session.set('current-keyword', ['peaceful']);
+			},
+			'I want to spend the rest of my life with you' () {
+
+				Session.set('current-phrase', 'I want to spend the rest of my life with you');
+				Session.set('current-keyword', ['pretty']);
+			},
+			'I miss you too' () {
+
+				Session.set('current-phrase', 'I miss you too');
+				Session.set('current-keyword', ['lost']);
+			},
+			'Heading out' () {
+
+				Session.set('current-phrase', 'Heading out');
+				Session.set('current-keyword', ['courage']);
+			},
+			'You are so beautiful' () {
+
+				Session.set('current-phrase', 'You are so beautiful');
+				Session.set('current-keyword', ['encouraged', 'playful']);
+			},
+			'I love you' () {
+
+				Session.set('current-phrase', 'I love you');
+				Session.set('current-keyword', ['love']);
+			},
+			'You make me want to be a better man' () {
+
+				Session.set('current-phrase', 'You make me want to be a better man');
+				Session.set('current-keyword', ['dreaming']);
+			},
+			'I want to marry you' () {
+
+				Session.set('current-phrase', 'I want to marry you');
+				Session.set('current-keyword', ['love']);
+			},
+			'Why are you not talking to me?' () {
+
+				Session.set('current-phrase', 'Why are you not talking to me?');
+				Session.set('current-keyword', ['love']);
+			},
+			'I want to go home with you' () {
+
+				Session.set('current-phrase', 'I want to go home with you');
+				Session.set('current-keyword', ['love']);
+			},
+			'You are my heart and my everything' () {
+
+				Session.set('current-phrase', 'You are my heart and my everything');
+				Session.set('current-keyword', ['end']);
+	      }
 	    });
 	    annyang.start();
 	}
